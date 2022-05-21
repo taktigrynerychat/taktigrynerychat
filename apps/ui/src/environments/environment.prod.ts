15b7eb2b135cc7f9c4e9dcd2a140fcd2496c4a97
Vlad <vladdaking@gmail.com>
2022-05-21 19:20:59 +0300

    ui, jungle-ui: fixed linting issues and added styles to jungle-ui assets

--- apps/ui/src/environments/environment.prod.ts
+++ apps/ui/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
-export const environment = {
+import { UiEnvironment } from './model';
+
+export const environment: UiEnvironment = {
   production: true,
 };
---
+++ apps/ui/src/environments/model.ts
@@ -0,0 +1,7 @@
+type UiEnvironment = {
+  production: boolean;
+}
+
+export {
+  UiEnvironment,
+};
